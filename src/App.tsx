5a78c3aa3ba45ff2339f4902861a7d640456cf33
Natalia Serradas <nataliaserradas@gmail.com>
2022-11-08 20:17:29 -0300

    inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg';
+import { Button } from './components/InfoItem/Button';
+import { InfoItem } from './components/InfoItem';
+//import { GridItemType } from './types/GridItemType';
 
-function App() {
+
+
+const App = () => {
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] =useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  //const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(() => resetAndCreateGrid(), []);
+
+  const resetAndCreateGrid = () => {
+    // passo 1 -resertar o jogo
+    setTimeElapsed(0);
+    setPlaying(false);
+    setMoveCount(0);
+   // setShownCount(0);
+    //setGridItems([]);
+
+    // passo 2 - criar o grid e começar o jogo
+    setPlaying(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+        
+        <C.InfoArea>
+          <InfoItem label="Tempo" value="00:00" />
+          <InfoItem label="Movimentos" value="0"/>
+        </C.InfoArea>
+
+        <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid} />
+      </C.Info>
+      <C.GridArea>
+      
+      </C.GridArea>
+    </C.Container>
   );
 }
 
 export default App;
+
+
---
+++ src/assets/js/script.js
@@ -0,0 +1,5 @@
+let nome = 'Bonieky Lacerda Leal';
+
+let resultado = nome.lreplace ('Lacerda', 'Silva');
+
+console.log(resultado);
